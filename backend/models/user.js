24ae41a292a3f5cdb0a4228a442cdb41af0183b6
Antonijo <antonijodev@gmail.com>
2019-11-14 15:17:11 +0000

    Added user schema

---
+++ backend/models/user.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// this will be our data base's data structure 
+const userSchema = new Schema ({
+  username: {type: String, required: true},
+	firstname: {type: String, required: true},
+	lastname: {type: String, required: true},
+  password: {type: String, required: true},
+  role: {type: String, required: true},
+});
+	
+
+
+// export the user schema
+module.exports = mongoose.model("User", userSchema);
--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 var cors = require('cors');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
-const Data = require('./data');
+const Data = require('./models/user');
 
 const API_PORT = 3001;
 const app = express();
